ec947f378ba3322b54f685593f9f89d7b1a0fed3
Coco All <corentin.all@gmail.com>
2016-12-19 22:20:25 +0100

    SOme shit have change

---
+++ CardGame/Assets/ListeActionsTextManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ListeActionsTextManager : MonoBehaviour {
+
+    public Text _actionsText = null;
+    public GameState _gS = 0;
+
+	// Use this for initialization
+	void Start () {
+        _gS = GameTurnManager._actualGameState;
+        UpdateText();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(_gS != GameTurnManager._actualGameState)
+        {
+            _gS = GameTurnManager._actualGameState;
+            UpdateText();
+        }
+	}
+
+
+    void UpdateText()
+    {
+        switch (_gS)
+        {
+            default:
+                Debug.Log("Cas de texte non gérer");
+                break;
+            case GameState.ChoisirAventurier:
+                _actionsText.text = "-Piocher une carte aventurier  -Choisir une carte aventurier   -Regarder les cartes défaussées";
+                break;
+            case GameState.PreparerDonjon:
+                _actionsText.text = "-Piocher une carte donjon      -Résoudre  le combat    -Regarder les cartes défaussées";
+                break;
+            case GameState.Combattre:
+                _actionsText.text = "";
+                break;
+            case GameState.Victoire:
+                _actionsText.text = "-Finir le tour    -Regarder cartes défausses";
+                break;
+            case GameState.Defaite:
+                _actionsText.text = "";
+                break;
+            case GameState.FinDeTour:
+                _actionsText.text = "";
+                break;
+            case GameState.CombatCritique1:
+                _actionsText.text = "";
+                break;
+            case GameState.CombatCritique2:
+                _actionsText.text = "";
+                break;
+        }
+    }
+}
--- CardGame/Assets/_Scripts/ChoosenAventurier.cs
+++ CardGame/Assets/_Scripts/ChoosenAventurier.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class ChoosenAventurier : MonoBehaviour {
 
+    //Stock la carte aventurier contre laquelle on se bat
     public GameObject _choosenOne = null;
 
 }
